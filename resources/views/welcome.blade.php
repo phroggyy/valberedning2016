358a24f97cac589d808915ad06a21fdcc0594d98
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-07-10 16:55:21 +0200

    Added some text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <title>Laravel</title>
 
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
@@ -19,13 +20,14 @@
                 font-family: 'Lato';
             }
 
-            .container {
+            .custom-container {
                 text-align: center;
                 display: table-cell;
                 vertical-align: middle;
             }
 
             .content {
+            	height: 100vh;
                 text-align: center;
                 display: inline-block;
             }
@@ -36,13 +38,19 @@
         </style>
     </head>
     <body>
-        <div class="container">
+        <div class="custom-container">
             <div class="content">
                 <div class="title">
                 	Valberedning 2016 <br/>
                 	<small><em>EUP Sverige</em></small>
             	</div>
             </div>
+        </div>
+        <div class="container">
+        	<div class="col-xs-12 col-sm-8 col-sm-offset-2">
+        		<h2 class="text-center">Valberedningens syfte</h2>
+        		<p>Vi som sitter i EUP Sveriges valberedning har i uppgift att rekommendera styrelsen för perioden 2016-2017 från de som kandiderat till styrelsen.</p>
+        	</div>
         </div>
     </body>
 </html>
